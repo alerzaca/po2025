31ac93e95ebe193ce10870c059b87d9ab13946d3
Aleksandra Rząca <arzaca@student.agh.edu.pl>
2025-10-13 19:57:22 +0200

    Added Lotto.java

---
+++ Lab2/Lotto.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Lotto {
+    public static void main(String[] args) {
+        // Check arguments and create a list of them
+        ArrayList<Integer> bets = new ArrayList<>();
+        if (args.length != 6) {
+            System.out.println("Not enough or too many arguments.");
+            return;
+        }
+        for (int i = 0; i < 6; i++) {
+            int arg = Integer.parseInt(args[i]);
+            if (bets.contains(arg)) {
+                System.out.println("Numbers cannot repeat.");
+                return;
+            }
+            bets.add(arg);
+        }
+        // Sort bets for comparison with winners
+        bets.sort(null);
+
+        // Generate lotto winners
+        Lotto lotto = new Lotto();
+        ArrayList<Integer> winners = new ArrayList<>();
+
+        // Compare winners with bets
+        long startTime = System.currentTimeMillis();
+        do {
+            winners = lotto.Lotto();
+            System.out.print("Generated numbers: ");
+            System.out.println(winners);
+            winners.sort(null);
+        } while (!winners.equals(bets));
+        long stopTime =  System.currentTimeMillis();
+
+        // Print results
+        System.out.print("Lotto: ");
+        System.out.println(winners);
+        System.out.print("\nGood bets number: ");
+        System.out.print(bets.size());
+        System.out.println("\nExecution time: " +  (stopTime - startTime));
+    }
+
+    public ArrayList<Integer> Lotto() {
+        ArrayList<Integer> winners = new ArrayList<>();
+        int randNum;
+        while (winners.size() < 6) {
+            randNum = (int) (Math.random() * 50);
+            if (!winners.contains(randNum)) {
+                winners.add(randNum);
+            }
+        }
+        return winners;
+    }
+}
